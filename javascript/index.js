24f52893a0ac5644dbbfe9501219b0c66e801766
anacris16 <titialtuve1605@gmail.com>
2023-04-28 14:33:42 +0200

    style of link fix

--- javascript/index.js
+++ javascript/index.js
@@ -49,7 +49,7 @@ function updateCity(event) {
             )} <small>${cityTime.format("A")}</small></div>
           </div>
         </div>
-        <a href="/">All cities</a>`;
+        <a  class="backHome" href="/">All cities</a>`;
 }
 
 let citiesSelectElement = document.querySelector("#city");
